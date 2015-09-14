36bded0c74b0c8b8fd1994d5321a447b01bd3463
Cory Spitzer & Dillon Lee <student@epicodus.com>
2015-09-14 09:59:30 -0700

    Add contacts

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+    var inputtedAddress = $("input#new-address").val();
+    console.log(inputtedAddress);
+    var newContact = { firstName: inputtedFirstName, lastName: inputtedLastName, address: inputtedAddress };
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + " " + newContact.lastName + "</span></li>");
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+    $("input#new-address").val("");
+  });
+});
