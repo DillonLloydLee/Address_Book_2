9d83af65c46abdda1451997761a9d9ce0f6a333c
Dillon Lloyd Lee <crazycapn@gmail.com>
2015-09-16 01:33:24 -0700

    add fullName spec to specs.js.

--- spec/specs.js
+++ spec/specs.js
@@ -1,10 +1,16 @@
 // Tests for functions:
 
 describe("Contact", function() {
+
   it("creates a new contact with the given specifications", function() {
     var testContact = new Contact("Rita", "Moreno");
     expect(testContact.firstName).to.equal("Rita");
     expect(testContact.lastName).to.equal("Moreno");
     expect(testContact.addresses).to.eql([]);
+  });
+
+  it ("adds the fullName method to all contacts", function() {
+    var testContact = new Contact("Sherlock", "Holmes");
+    expect(testContact.fullName()).to.equal("Sherlock Holmes");
   });
 });
